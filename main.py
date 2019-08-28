82cd236c2bdacb84289a8b17cfed7bd5d5daff0d
daisuke_toda <toda@thisco.jp>
2019-08-28 17:21:03 +0900

    globでwordsディレクトリの中身を定数に入れる修正

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pygame.locals import *
 import pygame
 import time
 import random
-
+import glob
 
 # 画面作成
 root = tk.Tk()
@@ -15,18 +15,18 @@ root.geometry('600x600')
 root.title('えいたんごのべんきょう')
 
 # 英単語定数
-ENGLISH_WORDS_LIST = ['dog','apple','elephant','cap','cat','paper','zebra','banana','money','butterfly','sleep','tree','number']
+ENGLISH_WORDS_LIST = glob.glob('words/*')
 
 # ボタンクリック
 def play():
-	filename = './words/' + random.choice(ENGLISH_WORDS_LIST) + '.mp3' #再生したいmp3ファイル
-	pygame.mixer.init()
-	pygame.mixer.music.load(filename) #音源を読み込み
-	mp3_length = mp3(filename).info.length #音源の長さ取得
-	pygame.mixer.music.play(1) #再生開始。1の部分を変えるとn回再生(その場合は次の行の秒数も×nすること)
-	time.sleep(mp3_length + 0.25) #再生開始後、音源の長さだけ待つ(0.25待つのは誤差解消)
-	pygame.mixer.music.stop() #音源の長さ待ったら再生停止
-        
+    filename = random.choice(ENGLISH_WORDS_LIST)
+    pygame.mixer.init()
+    pygame.mixer.music.load(filename)
+    mp3_length = mp3(filename).info.length 
+    pygame.mixer.music.play(1)
+    time.sleep(mp3_length + 0.25) 
+    pygame.mixer.music.stop()    
+    
 # ボタン
 btn = tk.Button(root, text='もんだい', command=play)
 btn.pack(fill = 'x', padx=40,pady=200)
